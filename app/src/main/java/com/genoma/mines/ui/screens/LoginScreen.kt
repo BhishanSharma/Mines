426f5494cc1b5718a77c6346c5811f22a8f5d7c9
bhishan-genoma <bhishan.builder@gmail.com>
2026-09-06 18:05:08 +0530

    added login screen preview

--- app/src/main/java/com/genoma/mines/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/genoma/mines/ui/screens/LoginScreen.kt
@@ -20,8 +20,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.genoma.mines.ui.theme.MinesTheme
+
 @Composable
 fun LoginScreen(
     onGoogleSignInClick: () -> Unit,
@@ -106,5 +109,16 @@ fun LoginScreen(
                 textAlign = TextAlign.Center
             )
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview(){
+    MinesTheme {
+        LoginScreen(
+            onGoogleSignInClick = {},
+            onGuestClick = {}
+        )
     }
 }
