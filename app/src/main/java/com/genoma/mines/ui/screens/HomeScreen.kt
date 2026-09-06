a7fb9298733d3b2316f3a0d2e96984647d41dcfd
alan-w-arch <himanshu.12322132@lpu.in>
2026-09-06 18:19:57 +0530

    Updated UI of LoginScreen.kt

--- app/src/main/java/com/genoma/mines/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/genoma/mines/ui/screens/HomeScreen.kt
@@ -27,7 +27,6 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.HelpOutline
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.TrackChanges
